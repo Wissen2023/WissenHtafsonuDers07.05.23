257e418784cf82f4ecc930e48544f7e204d8aa2d
dogukandurkaya <98876145+dogukandurkaya@users.noreply.github.com>
2023-05-07 12:19:55 +0300

    Add project files.

---
+++ WissenHtafsonuDers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WissenHtafsonuDers.wissen_ders;
+
+namespace WissenHtafsonuDers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Donguler donguler = new Donguler();
+            donguler.Ornekler();
+            //donguler.Ornekler2();
+        }
+    }
+}
---
+++ WissenHtafsonuDers/wissen_ders/Donguler.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WissenHtafsonuDers.wissen_ders
+{
+    public class Donguler
+    {
+        public void Ornekler()
+        {
+            //İsim Consola isim girdirip basalım
+            //Console.Write("Bir isim giriniz: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Girilen İsim: " + name);
+
+
+            //Konsola girilen 2 sayıyı topla, çıkar, çarp, böl
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Girilen sayı1 " + number1);
+            //Console.WriteLine("Girilen sayı1 " + number2);
+            //Console.WriteLine("Toplam " + (number1 + number2));
+            //Console.WriteLine("Fark " + (number1 - number2));
+            //Console.WriteLine("Çarpım " + (number1 * number2));
+            //Console.WriteLine("Bölüm " + (number1 / number2));
+
+
+            //Konsola Bir Sayı girdirelim bu sayı 100den büyükse sayı büyük yazsın 
+            //küçük ise sayı küçük yazsın
+            //Console.Write("Sayı Giriniz: ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //if (sayi > 100)
+            //{
+            //    Console.WriteLine("Sayı Büyük");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayı Küçük");
+            //}
+
+
+            //Console.WriteLine("Mevsim İsmi Giriniz: ");
+            //string mevsimler = Console.ReadLine();
+
+            //switch (mevsimler)
+            //{
+            //    case "Yaz":
+            //        Console.WriteLine("Bu mevsim yaz");
+            //        break;
+            //    case "Kış":
+            //        Console.WriteLine("Bu mevsim kış");
+            //        break;
+            //    case "Bahar":
+            //        Console.WriteLine("Bu mevsim bahar");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Mevsim ismi giriniz");
+            //        break;
+            //}
+
+            //Gün ismi giriniz ve girilen ismi konsola bastıralım
+
+            //Console.WriteLine("Gün İsmi Giriniz: ");
+            //string gunler = Console.ReadLine();
+
+            //switch (gunler)
+            //{
+            //    case "pazartesi":
+            //        Console.WriteLine("Bugün pazartesi");
+            //        break;
+            //    case "salı":
+            //        Console.WriteLine("Bugün salı");
+            //        break;
+            //    case "çarşamba":
+            //        Console.WriteLine("Bugün çarşamba");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Gün ismi giriniz");
+            //        break;
+            //}
+
+            //For Döngüsü
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //1den 10a kadar olan sayıları toplayalım
+            //int toplam = 0;
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    toplam = toplam + i;
+            //}
+            //Console.WriteLine("Toplam: " + toplam);
+
+
+            //1den 10a kadar olan çift sayıları toplayalım
+            //int toplam = 0;
+            //for (int i = 0; i <= 10; i+=2)
+            //{
+            //    toplam = toplam + i;
+            //}
+            //Console.WriteLine("Çift Sayıların Toplamı: " + toplam);
+
+
+            //5 e 5 kare ile * yazalım
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int number = 10;
+            //if (number == 9)
+            //{
+            //    Console.WriteLine("Sayı 9 a eşit");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayı 9 a eşit değil");
+            //}
+
+            //DateTime dateTime = DateTime.Now;
+            //if (dateTime.Day == 07)
+            //{
+            //    Console.WriteLine("Bugun ayın 7 si");
+            //}
+            //else if (true)
+            //{
+
+            //}
+            //else if (true)
+            //{
+
+            //}
+            //else if (true)
+            //{
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Bugün ayın 7 si değil");
+            //}
+
+            //bir sayı girdirelim girilen sayıyı bölenleri bulalaım 
+            //Console.Write("Lütfen bir sayı giriniz: ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 2; i < sayi; i++)
+            //{
+            //    if (sayi % i == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //int number = 1;
+
+            //while (number<10)
+            //{
+            //    Console.WriteLine(number);
+            //    number++;
+            //}
+
+            //int number = 1;
+            //int toplam = 0;
+            //while (number <= 10)
+            //{
+            //    toplam = toplam + number;
+            //    number++;
+            //}
+            //Console.WriteLine("Toplam : " + toplam);
+
+
+            //int number = -6;
+            //while (number == 100)
+            //{
+            //    Console.WriteLine(number);
+            //    number += 2;
+            //}
+
+            //int number = 1;
+            //do
+            //{
+            //    Console.WriteLine(number);
+            //    number++;
+            //} while (number == 10);
+
+            //int[] sayilar = new int[5];
+            //sayilar[0] = 1;
+            //sayilar[1] = 2;
+            //sayilar[2] = 3;
+            //sayilar[3] = 4;
+            //sayilar[4] = 5;
+
+            //int[] sayiler = new int[] { 1, 2, 3 };
+            //Console.WriteLine(sayiler[2]);
+
+            //var t = (1, 2, 3);
+            //Console.WriteLine(t.Item1);
+
+
+
+            Console.ReadLine();
+        }
+        //public void Ornekler2()
+        //{
+        //    Console.WriteLine("Burası örnekler 2");
+        //    Console.ReadLine();
+        //}
+    }
+
+}
